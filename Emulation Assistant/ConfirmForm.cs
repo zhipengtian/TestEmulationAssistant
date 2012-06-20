ec05221aa0338e5375639dd3a2a0d4afe16d1af6
gwatt <gwatt@indiana.edu>
2012-06-20 15:03:18 -0400

    Added in the original project so that this solution now contains both projects, for simplicity

---
+++ Emulation Assistant/ConfirmForm.cs
@@ -0,0 +1,61 @@
+﻿/*
+Copyright 2007 Indiana University Research and Technology Corporation Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License. 
+*/
+
+//=====================================
+//_________ - Mitchell Lutz - _________
+//            Summer 2009             
+//=====================================
+
+using System.Windows.Forms;
+using System.IO;
+using System;
+using System.Diagnostics;
+
+namespace AFSBrowser {
+    public partial class ConfirmForm: Form {
+        private PathsForm myParent;
+
+        //-------------------------------------------------------------
+        // ConfirmForm Constructor
+        //-------------------------------------------------------------
+        public ConfirmForm(PathsForm parent) {
+            InitializeComponent();
+
+            this.myParent = parent;
+
+            this.txtBoxISOpath.Text = parent.getISOpath(); // Set file or image path in text box
+            //this.isoID = Path.GetFileNameWithoutExtension(this.txtBoxISOpath.Text); // isoID Example: 30000038669341
+
+            // installScriptPath Example: \\afs\iu.edu\public\sudoc\volumes\04\30000038669341\30000038669341_install.exe
+            //this.installScriptPath = Path.Combine(Path.GetDirectoryName(this.txtBoxISOpath.Text), this.isoID + "_install.exe");
+
+            if(parent.getServiceProvider() == 1)
+                this.txtBoxProvider.Text = "Workstation";
+            else 
+                this.txtBoxProvider.Text = "Server";
+        }
+
+        //-------------------------------------------------------------
+        // Open button event
+        //-------------------------------------------------------------
+        private void btnContinue_Click(object sender, System.EventArgs e) {
+            this.TopMost = false;
+            this.myParent.TopMost = true;
+            this.myParent.SetPassword(this.txtPassword.Text);
+            this.myParent.StartSetup();
+            this.myParent.btnCancel_SetEnabled(true);
+        }
+
+        //---------------------------------------------------------------
+        // Cancel button event
+        //---------------------------------------------------------------
+        // Closes this.ConfirmForm, goes back to PathsForm.
+        private void btnCancel_Click(object sender, System.EventArgs e) {
+            this.TopMost = false;
+            this.myParent.TopMost = true;
+            this.Dispose();
+            this.Close();
+        }
+    }
+}
---
+++ ISO Display/ISODisplay.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace EmulationAssistant
+{
+	public partial class ISODisplay : Form
+	{
+		private const String AFSPath = "";
+		public ISODisplay()
+		{
+			InitializeComponent();
+		}
+
+		private void downloadXmlButton_Click(object sender, EventArgs e)
+		{
+			if (String.IsNullOrEmpty(XmlUrlBox.Text))
+			{
+				MessageBox.Show("Please enter a valid URL");
+				return;
+			}
+			while (CDDisplay.Rows.Count > 0)
+			{
+				CDDisplay.Rows.RemoveAt(0);
+			}
+			List<CDInfo> cds = CDInfo.GetCDInfo(XmlUrlBox.Text);
+
+			foreach (CDInfo cd in cds)
+			{
+				CDDisplay.Rows.Add(new DataGridCDRow(cd));
+			}
+		}
+
+		private void exePathBox_DoubleClick(object sender, EventArgs e)
+		{
+			if (exePathDialog.ShowDialog() == DialogResult.OK)
+			{
+				exePathBox.Text = exePathDialog.FileName;
+			}
+		}
+
+		private void launchVM_Click(object sender, EventArgs e)
+		{
+			CDInfo cd;
+			if (CDDisplay.SelectedRows.Count > 1)
+			{
+				MessageBox.Show("Please Select a CD to use");
+				return;
+			}
+			cd = ((DataGridCDRow)CDDisplay.SelectedRows[0]).Cd;
+			System.Diagnostics.ProcessStartInfo procStartInfo = new System.Diagnostics.ProcessStartInfo(exePathBox.Text, AFSPath+"\\"+cd.Isbn+".iso");
+			System.Diagnostics.Process proc = new System.Diagnostics.Process();
+			proc.StartInfo = procStartInfo;
+			proc.Start();
+		}
+
+		//
+		// For when this part of the emulation assistant is able to run without using
+		// the previous emulation assistant as well . . . . 
+		//
+		//
+		/*private void launchVM_Click(object sender, EventArgs e)
+		{
+			CDInfo cd;
+			if (CDDisplay.SelectedRows.Count == 0)
+			{
+				MessageBox.Show("Please select a product from the list on the right");
+				return;
+			}
+			cd = ((DataGridCDRow)CDDisplay.SelectedRows[0]).Cd;
+			VixWrapper vmware = new VixWrapper();
+			if (!vmware.Connect(null, null, null))
+			{
+				return;
+			}
+			
+			if (!vmware.Open(IsoLocationBox.Text))
+			{
+				return;
+			}
+			//vmware.RevertToLastSnapshot();
+			if (!vmware.PowerOn())
+			{
+			}
+		}*/
+
+	}
+
+	public class DataGridCDRow : DataGridViewRow
+	{
+		public CDInfo Cd;
+
+		public DataGridCDRow() : base() {}
+
+		public DataGridCDRow(CDInfo cd) : base()
+		{
+			this.Cells.Add(new DataGridCDCell(cd.Isbn));
+			this.Cells.Add(new DataGridCDCell(cd.Name));
+			this.Cells.Add(new DataGridCDCell(cd.Description));
+			this.Cells.Add(new DataGridCDCell(cd.Author));
+			this.Cells.Add(new DataGridCDCell(cd.Year));
+			this.Cd = cd;
+		}
+	}
+
+	public class DataGridCDCell : DataGridViewTextBoxCell
+	{
+		public DataGridCDCell() : base() { }
+		public DataGridCDCell(String Value) : this()
+		{
+			this.Value = Value;
+		}
+	}
+}
